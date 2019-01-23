a9351b3f9ead30b408a3ff439bc3c631cc747989
Werstern <werstern@bigmir.net>
2019-01-23 22:07:51 +0200

    mini project finished

---
+++ dist/precache-manifest.7e8257421c478b72439a9c4237040cdb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "58ebc3825f160382c009",
+    "url": "/static/js/main.58ebc382.chunk.js"
+  },
+  {
+    "revision": "560cd78e6c71bcf6a9d8",
+    "url": "/static/js/1.560cd78e.chunk.js"
+  },
+  {
+    "revision": "58ebc3825f160382c009",
+    "url": "/static/css/main.11022489.chunk.css"
+  },
+  {
+    "revision": "46c37a90066dd0040d58e8cae8292f6d",
+    "url": "/index.html"
+  }
+];
